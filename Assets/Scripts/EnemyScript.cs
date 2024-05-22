d789a23e6219105704d3a4c725de40ae9460bf14
bambaisbanned <Iphonegamer400@gmail.com>
2024-05-22 13:06:07 +0200

    adding enemy script

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // this does wonders
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("i am a banana");
+    }
+}
