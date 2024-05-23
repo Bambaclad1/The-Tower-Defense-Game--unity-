e0b754bab118f07a36d5c494a6199a7d71be52f7
bambaisbanned <Iphonegamer400@gmail.com>
2024-05-23 12:25:13 +0200

    actual enemy script

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public bool HP;
+    public bool ATK; 
+
     void Start()
     {
         // this does wonders
@@ -16,9 +19,7 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print("i am a banana");
-        print("I am a dangerous script booho!");
-        print("THIS HAS BEEN DONE IN THE MAIN BRANCH AND THE ENEMYMOVEMENT SCRIPT!!");
+
 
     }
 }
